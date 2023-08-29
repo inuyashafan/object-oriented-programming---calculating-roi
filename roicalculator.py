366b19c3376d91288c648aa286225f34dc6fdc77
John Abreu <johnabreu67@gmail.com>
2023-08-28 21:11:10 -0500

    second commit

--- roicalculator.py
+++ roicalculator.py
@@ -1,18 +1,44 @@
 class roi_rental():
     """calculate the return on investment of a rental property"""
-    def __init__(self, income, expenses, investment):
+    def __init__(self, income, expenses, cashflow, investment):
         self.income = income
+        self.cashflow = cashflow
         self.expenses = expenses
         self.investment = investment
+    def calc_income(self, r_i, laundry, storage, misc):
+        self.income = r_i + laundry + storage + misc 
+        print(self.income) 
+    def calc_expenses(self, tax, insurance, util, HOA, lawnsnow, vacancy, repairs, capex, propmanage, mort):
+        self.expenses = tax + insurance + util + HOA + lawnsnow + vacancy + repairs + capex + propmanage + mort
+        print(self.expenses)
+    def calc_cashflow(self):
+        self.cashflow = self.income - self.expenses
+        print(self.cashflow)
+    def calc_investment(self, downpay, closing, rehab, misc):
+        self.investment = downpay + closing + rehab + misc
+        print(self.investment)
+
     def calc_roi(self):
-        mc = self.income - self.expenses
-        roi = mc * 12 / self.investment
-        print(f"{roi * 100}%") 
+        roi = (self.cashflow*12) / self.investment
+        print(f"{roi*100} %")
+
+
+better_pockets = roi_rental(0, 0, 0, 0)
+
+better_pockets.calc_income(2000, 0, 0, 0)
+
+better_pockets.calc_expenses(150,100, 0, 0, 0, 100, 100, 100, 200, 860)
+
+better_pockets.calc_investment(40000, 3000, 7000, 0)
+
+better_pockets.calc_cashflow()
+
+better_pockets.calc_roi()
+
+        
     
     
     
-my_rental_property = roi_rental(2000, 1610, 50000)
-my_rental_property.calc_roi()
 
         
 
